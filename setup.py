7963de6144dff0c468ecc4eb2ea1a5c158b620c1
albi-c <albert.csontos@gmail.com>
2022-05-31 14:34:00 +0200

    Increased version to 1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="mlogpp",
-    version="1.3",
+    version="1.4",
     author="albi-c",
     description="mlog++ to mindustry logic compiler",
     long_description=long_description,
