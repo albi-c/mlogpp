5ca96066d2eb0b4eb9f30bcd4d87db256755a9bf
albi-c <albert.csontos@gmail.com>
2022-06-03 15:29:08 +0200

    Progress on optimizer

--- mlogpp/optimizer.py
+++ mlogpp/optimizer.py
@@ -9,7 +9,12 @@ class Optimizer:
 
     REGEXES = {
         # setting temporary variable
-        "SU_SET": re.compile(r"^set __tmp\d+ .+$")
+        "TMP_SET": re.compile(r"^set __tmp\d+ .+$"),\
+        # reading to temporary variable
+        "TMP_READ": re.compile(r"^read __tmp\d+ \S+ \S+$"),
+
+        # assigning from temporary variable
+        "TA_SET": re.compile(r"^set [a-zA-Z_@][a-zA-Z_0-9]* __tmp\d+$")
     }
 
     def optimize(code: str) -> str:
@@ -17,15 +22,12 @@ class Optimizer:
         optimize generated code
         """
 
-        while True:
-            code, changed = Optimizer._single_use_tmp(code)
-
-            if not changed:
-                break
+        for i in range(1, 1025):
+            code, _ = Optimizer._single_use_tmp(code, i)
         
         return code
 
-    def _single_use_tmp(code: str) -> str:
+    def _single_use_tmp(code: str, forward: int = 1) -> str:
         """
         optimize single use temporary variables
         """
@@ -36,25 +38,37 @@ class Optimizer:
             if c > 0:
                 uses[f"__tmp{i}"] = c
         
-        changed = False
-        values = {}
+        found = False
+        lns = code.splitlines()
         tmp = ""
-        for i, ln in enumerate(code.splitlines()):
-            if Optimizer.REGEXES["SU_SET"].fullmatch(ln):
+        for i, ln in enumerate(lns):
+            fi = i + forward
+
+            if Optimizer.REGEXES["TMP_SET"].fullmatch(ln):
                 spl = ln.split(" ", 2)
-                if uses.get(spl[1], -1) == 2:
-                    values[spl[1]] = spl[2]
-                    changed = True
-                    continue
+                name = spl[1]
+                val = spl[2]
+
+                if i < len(lns) - forward:
+                    if uses.get(name, 0) == 2 and name in lns[fi]:
+                        print(f"TMP_SET({forward}) 1   {lns[fi]}")
+                        lns[fi] = " ".join([val if part == name else part for part in lns[fi].split(" ")])
+                        print(f"TMP_SET({forward}) 2   {lns[fi]}")
+                        found = True
+                        continue
             
-            # tmp += " ".join([values.get(part, part) for part in ln.split(" ")]) + "\n"
+            elif Optimizer.REGEXES["TMP_READ"].fullmatch(ln):
+                spl = ln.split(" ", 3)
+                name = spl[1]
+
+                if i < len(lns) - forward:
+                    if uses.get(name, 0) == 2 and Optimizer.REGEXES["TA_SET"].fullmatch(lns[fi]):
+                        spl_ = lns[fi].split(" ", 2)
+                        if name == spl_[2]:
+                            lns[fi] = f"sensor {spl_[1]} {spl[2]} {spl[3]}"
+                            found = True
+                            continue
+            
             tmp += ln + "\n"
         
-        print(values)
-
-        code = ""
-        for i, ln in enumerate(tmp.strip().splitlines()):
-            # code += ln + "\n"
-            code += " ".join([values.get(part, part) for part in ln.split(" ")]) + "\n"
-        
-        return code.strip(), changed
+        return tmp.strip(), found
